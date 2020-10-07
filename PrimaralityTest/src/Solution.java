67698acc91a1c9fcb2f1d92a46ce548535a99519
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2020-10-07 16:50:19 +0530

    Add files via upload

---
+++ PrimaralityTest/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+       /* System.out.println(scanner.nextBigInteger().isProbablePrime(100) ? "prime" : "not prime");
+        scanner.close();*/
+        System.out.println(10/5);
+        System.out.println(Math.pow(2,8));
+    }
+}
