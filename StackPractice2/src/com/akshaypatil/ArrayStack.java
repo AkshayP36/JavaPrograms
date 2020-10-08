ac0b3c9c8dd124352d3e59b675ecb14692db70b4
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2020-10-08 18:26:57 +0530

    Add files via upload

---
+++ StackPractice2/src/com/akshaypatil/ArrayStack.java
@@ -0,0 +1,4 @@
+package com.akshaypatil;
+
+public class ArrayStack {
+}
---
+++ StackPractice2/src/com/akshaypatil/Library.java
@@ -0,0 +1,24 @@
+package com.akshaypatil;
+
+public class Library {
+    private String bookName;
+    private String autherName;
+    private int bookNumber;
+
+    public Library(String bookName, String autherName, int bookNumber){
+        this.bookName=bookName;
+        this.autherName=autherName;
+        this.bookNumber=bookNumber;
+    }
+
+    public void setBookName(String bookName){
+        this.bookName=bookName;
+    }
+    public void setAutherName(String autherName){
+        this.autherName=autherName;
+    }
+    public void setBookNumber(int bookNumber){
+        this.bookNumber=bookNumber;
+    }
+    public String
+}
