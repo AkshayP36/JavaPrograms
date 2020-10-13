9d3978ab16b9759ea6a0d5de1f01a386125315aa
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2020-10-13 22:23:44 +0530

    Add files via upload

---
+++ HelloWorld/demo/src/main/java/com/akshaypatil/demo/HomeController.java
@@ -0,0 +1,13 @@
+package com.akshaypatil.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello, Akshay from Spring project!";
+    }
+}
