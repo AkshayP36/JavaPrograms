af2a5cf5550748dde93626291138f777dc7ae4e2
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2020-10-25 20:14:51 +0530

    Add files via upload

---
+++ RBGQueries/src/com/akshaypatil/Main.java
@@ -0,0 +1,35 @@
+package com.akshaypatil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Result {
+
+    /*
+     * Complete the 'mixColors' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. 2D_INTEGER_ARRAY colors
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<String> mixColors(List<List<Integer>> colors, List<List<Integer>> queries) {
+
+        // declaring ans string
+        List<String> ans = new ArrayList<>();
+
+        // finding
+
+
+        return ans;
+
+    }
+
+}
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
