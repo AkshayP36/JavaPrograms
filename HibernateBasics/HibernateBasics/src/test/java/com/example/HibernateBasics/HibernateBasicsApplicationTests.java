bf812cb1893c86a2698063ac526ee78e477c6890
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2020-11-01 15:14:25 +0530

    Add files via upload

---
+++ HibernateBasics/HibernateBasics/src/test/java/com/example/HibernateBasics/HibernateBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HibernateBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
